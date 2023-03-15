8addbcc7e6c23ac1b628fa1633c3521be41ad6c6
vaceslavkameronov <slka@mail.ru>
2023-03-16 00:39:38 +0400

    решение ДЗ

---
+++ src/Bicycle.java
@@ -0,0 +1,11 @@
+public class Bicycle extends Technic implements TypeWorkTyre {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку Bicycle");
+    }
+}
---
+++ src/TypeWorkTyre.java
@@ -0,0 +1,3 @@
+public interface TypeWorkTyre {
+    void updateTyre();
+}
